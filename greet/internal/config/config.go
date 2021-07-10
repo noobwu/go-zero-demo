5e0fc7e3684a154423a3c1bbb76084ef7c94630a
guoguo <noobwu@126.com>
2021-07-10 14:42:07 +0800

    user jwt config update
    user jwt config update

--- greet/internal/config/config.go
+++ greet/internal/config/config.go
@@ -2,8 +2,17 @@
 
 package config
 
-import "github.com/tal-tech/go-zero/rest"
+import (
+	"github.com/tal-tech/go-zero/core/stores/cache"
+	"github.com/tal-tech/go-zero/rest"
+)
 
 type Config struct {
-	rest.RestConf
+	rest.RestConf                 // rest api配置
+	CacheRedis    cache.CacheConf // redis缓存
+	Auth          struct {        // jwt鉴权配置
+
+		AccessSecret string
+		AccessExpire int64
+	}
 }
